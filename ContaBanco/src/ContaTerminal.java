c512206cd396f87710490b33e87c4bee3fe5cf1d
RuanCostaDev <ruanrrc@hotmail.com>
2024-04-24 21:38:26 -0300

    Inicio do projeto Conta Bancária no Terminal

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,10 @@
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        //TODO: Revise sobre terminal, main args e a classe Scanner
+        //TODO: Permitir Inserção dos dados do usurário no terminal(numero, agencia, nomeCliente, Saldo)
+
+        //TODO: Exibir os dados no seguinte formato:
+        //"Olá [Nome Cliente], obrigado por criar uma conta em nosso banco, sua agência é [Agencia], 
+        //conta [Numero] e seu saldo [Saldo] já está disponível para saque".
+    }
+}
