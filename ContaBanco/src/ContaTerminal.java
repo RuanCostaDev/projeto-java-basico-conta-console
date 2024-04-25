0aa4fec518c2e5bec9e42d6f078ec126d22f846a
RuanCostaDev <ruanrrc@hotmail.com>
2024-04-24 21:46:18 -0300

    Atualizando o TODO

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,10 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
+        //Atributo	Tipo	Exemplo
+        //Numero	Inteiro	1021
+        //Agencia	Texto	067-8
+        //Nome Cliente	Texto	MARIO ANDRADE
+        //Saldo	Decimal	237.48
         //TODO: Revise sobre terminal, main args e a classe Scanner
         //TODO: Permitir Inserção dos dados do usurário no terminal(numero, agencia, nomeCliente, Saldo)
 
